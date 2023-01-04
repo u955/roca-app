ce8f77dfe3f301965c98a74888154a2d516475cd
u955 <u_u955Pb6u@protonmail.com>
2023-01-04 15:32:14 +0900

    welcome viewのルートを削除

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 Route::get('/', fn() => view('index'))->name('index');
 Route::get('livechat',  'LiveChat@index')->name('livechat');
 
