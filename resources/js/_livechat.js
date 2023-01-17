171200f955af9478b6eda7a01cc853fb55fe5b90
u955 <u_u955Pb6u@protonmail.com>
2023-01-17 21:21:48 +0900

    livechat_streamingの機能削減

--- resources/js/_livechat.js
+++ resources/js/_livechat.js
@@ -1,33 +1,27 @@
 
-// MODULES
-import * as cookieModule from './cookie';
-import * as cleanerModule from './cleaner';
-
-
 // MEMBERS
 var user_key;
-var livechat_id;
 
 
 // MAIN
 $(function() {
     user_key = window.location.search.slice(1).replace('user_key=', ''); // -> URLクエリの取得
-    getLiveChatID();
+    getLiveBroadcast();
 });
 
 
 // ----- streaming ----- //
 
 // livechatidの取得(LiveChat.php)
-function getLiveChatID() {
+function getLiveBroadcast() {
     $.ajax({
-        url: `/api/getlivechatid?userkey=${user_key}`,
+        url: `/api/getlivebroadcast?userkey=${user_key}`,
         dataType: 'json'
     })
     .then(
         data => {
-            livechat_id = data;
-            streaming(); // -> streaming開始
+            // -> livechat表示
+            window.location.href(`https://www.youtube.com/live_chat?v=${data.videoid}`);
         },
         error => {
             setFaildMessage();
@@ -35,58 +29,7 @@ function getLiveChatID() {
 }
 
 
-// livechatの取得(LiveChat.php)
-var streaming = function() {
-    const pageToken = cookieModule.get('pageToken'); // -> cookieを参照
-
-    $.ajax({
-        // -> pageTokenがNullの場合''を返す
-        url: `/api/streaming?userkey=${user_key}&livechatid=${livechat_id}&pageToken=${pageToken ?? ''}`,
-        dataType: 'json'
-    })
-    .then(
-        data => {
-            setLiveChatMessage(data);
-            document.cookie = `pageToken=${data.nextPageToken}; max-age=30` // -> cookieを登録
-            setTimeout('streaming()', 3000); // -> 同期間隔の設定
-        },
-        error => {
-            setTimeout('streaming()', 3000); // -> 同期間隔の設定
-        }
-    );
-}
-
-
-// ----- display ----- //
-
-// livechatMessageの反映
-function setLiveChatMessage(data) {
-    data.items.forEach(element => {
-        const profileImageUrl = element.authorDetails.profileImageUrl;
-        const isChatSponsor = (element.authorDetails.isChatSponsor == 1)? 'moderator' : '';
-        const displayName = element.authorDetails.displayName;
-        const displayMessage = cleanerModule.convert(user_key, element);
-
-        var html =`
-        <img class='avatar' src='${profileImageUrl}'>
-
-        <p class='displaymessage'>
-            <span class='displayname ${isChatSponsor}'>${displayName}</span>
-            ${displayMessage}
-        </p>
-        `;
-
-        html = `<div class='chat'>${html}</div>`;
-        $('#livechat').append(html);
-    });
-
-    // -> liveChatの最下部まで移動
-    let target = document.getElementById('livechat');
-    target.scrollIntoView(false);
-}
-
-
-// getLiveChatIDが失敗した時の処理
+// getLiveBroadcastが失敗した時の処理
 function setFaildMessage() {
 
     var html = `
