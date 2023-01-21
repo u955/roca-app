de9863e6463e023b4713c9acad5bd1ec4c31a558
u955 <u_u955Pb6u@protonmail.com>
2023-01-21 22:11:46 +0900

    evalログ・API連結実装

--- app/Http/Controllers/LiveChat.php
+++ app/Http/Controllers/LiveChat.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\AccessToken;
 use App\Models\APICilent;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class LiveChat extends Controller
 {
@@ -17,56 +16,129 @@ class LiveChat extends Controller
         $this->key = config('services.youtube.api_key');
     }
 
-
-    // ACCESS FROM URL
     public function index(Request $request)
     {
         // -> URLクエリからuser_keyを取得
         $user_key = $request->user_key;
-        return view('livechat' , compact('user_key'));
+        if($request->path() == 'livechat') {
+            return view('livechat' , compact('user_key'));
+        }else {
+            return view('eval' , compact('user_key'));
+        }
     }
 
 
-    // livechatidを返すAPI
-    public function getLiveChatID(Request $request)
+    // ----- API ----- //
+
+    // liveBroadcastを返すAPI
+    public function getLiveBroadcast(Request $request)
     {
+        // -> user_keyが存在しなければ処理しない
+        if(AccessToken::userKeyDoesntExist($request->user_key)) return;
+
         $access_token = AccessToken::get($request->userkey); // -> user_keyからaccess_tokenを取得
         $url = "https://youtube.googleapis.com/youtube/v3/liveBroadcasts?part=snippet&broadcastStatus=active&maxResults=1&access_token={$access_token}&key={$this->key}";
 
         // -> API呼び出し
         $liveStreamingDetails = APICilent::APIgetJSON($url, 'GET');
         $liveChatID = $liveStreamingDetails['items'][0]['snippet']['liveChatId'];
-        return response()->json($liveChatID);
+        $videoID = $liveStreamingDetails['items'][0]['id'];
+
+        return response()->json([
+            'livechatid'=> $liveChatID,
+            'videoid' => $videoID
+        ]);
     }
 
 
-    // liveChatを取得するAPI
-    public function streaming(Request $request)
+    // liveChatを監視するAPI
+    public function monitoring(Request $request)
     {
-        // -> user_keyが一致しなければ処理しない
-        if(DB::table('users')->where('user_key', $request->userkey)->doesntExist()) return;
+        // -> user_keyが存在しなければ処理しない
+        if(AccessToken::userKeyDoesntExist($request->user_key)) return;
 
-        $id = $request->livechatid; // -> URLクエリからliveChatIdを取得
+        $livechatid = $request->livechatid; // -> URLクエリからliveChatIdを取得
         $pageToken = $request->pageToken; // -> URLクエリからpageTokenを取得
 
+        $livechat = $this->streaming($livechatid, $pageToken);
+        $livechat['items'] = $this->evalLiveChatMessages($livechat);
+        return response()->json($livechat);
+
+        // ------------------------log出力
+    }
+
+
+    // ----- Model: Evaluation ----- //
+
+    // livechatの評価
+    private function evalLiveChatMessages($data, $evaluated=[])
+    {
+        // dataからdisplayMessageのみを取り出し
+        $texts = array_map(fn($item): string =>
+            $item['snippet']['displayMessage'], $data['items']);
+
+        // textsをAIに評価させる
+        $results = $this->evalTexts($texts);
+
+        // 評価を基に判定
+        foreach($results['body'] as $i => $result) {
+            $neutral = $result['score']['neutral'];
+
+            // neutralとの差で判定する
+            $calc = [
+                'neutral'     => $neutral - $neutral,
+                'slander'     => $neutral - $result['score']['slander'],
+                'sarcasm'     => $neutral - $result['score']['sarcasm'],
+                'sexual'      => $neutral - $result['score']['sexual'],
+                'spam'        => $neutral - $result['score']['spam'],
+                'divulgation' => $neutral - $result['score']['divulgation']
+            ];
+            // 差の値が最小の項目が判定値となる
+            $judgement = array_search(min($calc), $result);
+            $result += Array('judgement' => $judgement);
+            array_push($evaluated, $data['items'][$i] + $result);
+        }
+        return $evaluated;
+
+        // ------------------------削除・対処処理
+    }
+
+
+    // ----- Model: Roca API ----- //
+
+    private function evalTexts($texts)
+    {
+        $url = "https://ivqjs20obe.execute-api.us-east-1.amazonaws.com/roca-api-apigateway-eval-stage";
+        $option = ['json' => ['texts' => $texts]];
+
+        // -> API呼び出し
+        $results = APICilent::APIgetJSON($url, 'POST', $option);
+        return $results;
+    }
+
+
+    // ----- Model: YouTube API ----- //
+
+    // liveChatの取得
+    private function streaming($id, $pageToken)
+    {
         $url = "https://youtube.googleapis.com/youtube/v3/liveChat/messages?part=snippet%2CauthorDetails&liveChatId={$id}&key={$this->key}";
         if(!is_null($pageToken)) $url .= "&pageToken={$pageToken}"; // -> pageTokenがnullじゃないとき、pageTokenクエリを挿入
 
         // -> API呼び出し
         $liveChat = APICilent::APIgetJSON($url, 'GET');
-        return response()->json($liveChat);
+        return $liveChat;
     }
 
 
-    // livechatMessageを削除するAPI
-    public function deleteLiveChatMessage(Request $request)
+    // livechatMessageの削除
+    private function deleteLiveChatMessage($id, $user_key)
     {
-        $id = $request->id; // -> livechatmessageidを取得
-        $access_token = AccessToken::get($request->user_key); // -> user_keyからaccess_tokenを取得
+        $access_token = AccessToken::get($user_key); // -> user_keyからaccess_tokenを取得
         $url = "https://youtube.googleapis.com/youtube/v3/liveChat/messages?id={$id}&access_token={$access_token}&key={$this->key}";
 
         // -> API呼び出し
         $result = APICilent::APIgetJSON($url, 'DELETE');
-        return response()->json($result);
+        return $result;
     }
 }
---
+++ resources/js/_eval.js
@@ -0,0 +1,99 @@
+
+// MODULES
+import * as cookieModule from './cookie';
+
+
+// MEMBERS
+var user_key;
+var livechat_id;
+
+
+// MAIN
+$(function() {
+    user_key = window.location.search.slice(1).replace('user_key=', ''); // -> URLクエリの取得
+    getLiveBroadcast();
+});
+
+
+// ----- streaming ----- //
+
+// livechatidの取得(LiveChat.php)
+function getLiveBroadcast() {
+    $.ajax({
+        url: `/api/getlivebroadcast?userkey=${user_key}`,
+        dataType: 'json'
+    })
+    .then(
+        data => {
+            livechat_id = data.livechatid;
+            monitoring(); // -> streaming開始
+        },
+        error => {
+            setFaildMessage();
+    });
+}
+
+
+// livechatの取得(LiveChat.php)
+const monitoring = function() {
+    const pageToken = cookieModule.get('pageToken'); // -> cookieを参照
+
+    $.ajax({
+        // -> pageTokenがNullの場合''を返す
+        url: `/api/monitoring?userkey=${user_key}&livechatid=${livechat_id}&pageToken=${pageToken ?? ''}`,
+        dataType: 'json'
+    })
+    .then(
+        data => {
+            setLiveChatMessage(data); // -> ログの表示
+            document.cookie = `pageToken=${data.nextPageToken}; max-age=30` // -> cookieを登録
+            setTimeout(monitoring, 3000); // -> 同期間隔の設定
+        },
+        error => {
+            setTimeout(monitoring, 3000); // -> 同期間隔の設定
+        }
+    );
+}
+
+
+// ----- display ----- //
+
+// livechatMessageの反映
+function setLiveChatMessage(data) {
+    data.items.forEach(element => {
+        const profileImageUrl = element.authorDetails.profileImageUrl;
+        const isChatSponsor = (element.authorDetails.isChatSponsor == 1)? 'moderator' : '';
+        const displayName = element.authorDetails.displayName;
+        const displayMessage = element.authorDetails.displayMessage;
+
+        var html =`
+        <img class='avatar' src='${profileImageUrl}'>
+
+        <p class='displaymessage'>
+            <span class='displayname ${isChatSponsor}'>${displayName}</span>
+            ${displayMessage}
+        </p>
+        `;
+
+        html = `<div class='chat'>${html}</div>`;
+        $('#log').append(html);
+    });
+
+    // -> liveChatの最下部まで移動
+    let target = document.getElementById('log');
+    target.scrollIntoView(false);
+}
+
+
+// getLiveBroadcastが失敗した時の処理
+function setFaildMessage() {
+
+    var html = `
+    <p class='failed'>
+        配信が開始されると、ここにログが表示されます。<br>
+        <a href="${location.href}">再読み込み</a>
+    </p>
+    `;
+
+    $('#log').append(html);
+}
